ccfb6379bb72467b620fd397f6c2b5a410d36d87
Xing <963255230@qq.com>
2019-12-02 17:19:54 +0800

    import '../lib/weui/weui.css'

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './index'
 
+import '../lib/weui/weui.css'
+
 // add this to handle exception
 Vue.config.errorHandler = function (err) {
   if (console && console.error) {
