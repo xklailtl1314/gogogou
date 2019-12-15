9265a17bdd1757eff9bc43e72014aeb02dae2ff9
Xing <963255230@qq.com>
2019-12-15 10:32:27 +0800

    首页-swiper块-样式修改

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="swiper-wrap">
-    <swiper indicator-dots interval="2000" duration="500" autoplay indicator-color="#fff" indicator-active-color="rgba(255,255,255,.3)">
-      <block v-for="(item,idx) in swipers" :key="idx">
-        <swiper-item>
-          <view class="swiper-item">
-            <image mode="widthFix" :src="item" />
-          </view>
-        </swiper-item>
-      </block>
-    </swiper>
+    <div class="bg"></div>
+    <div class="wrap">
+      <swiper indicator-dots interval="2000" duration="500" autoplay indicator-color="#fff" indicator-active-color="rgba(255,255,255,.3)">
+        <block v-for="(item,idx) in swipers" :key="idx">
+          <swiper-item>
+            <view class="swiper-item">
+              <image mode="widthFix" :src="item" />
+            </view>
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
   </div>
 </template>
 
@@ -20,17 +23,26 @@ export default {
 
 <style lang="scss" scoped>
 .swiper-wrap {
-  padding: 20rpx 30rpx 0;
-  // line-height: 10rpx;
-  background-color: #fff;
-  swiper {
-    height: 306rpx;
-    width: 690rpx;
-    border-radius: 10rpx;
-    overflow: hidden;
-    swiper-item {
-      image {
-        width: 100%;
+  position: relative;
+  .bg {
+    position: absolute;
+    height: 153rpx;
+    width: 100%;
+    background-color: #b51c66;
+  }
+  .wrap {
+    padding: 24rpx 30rpx 0;
+    // line-height: 10rpx;
+    background-color: #fff;
+    swiper {
+      height: 306rpx;
+      width: 690rpx;
+      border-radius: 10rpx;
+      overflow: hidden;
+      swiper-item {
+        image {
+          width: 100%;
+        }
       }
     }
   }
