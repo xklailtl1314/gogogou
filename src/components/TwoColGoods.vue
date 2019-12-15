a66cb2de3e6092e78fcc1c8ca65c5f7a968a7d48
Xing <963255230@qq.com>
2019-12-15 10:33:08 +0800

    两列布局块-添加

---
+++ src/components/TwoColGoods.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="two-clo-goods">
+    <navigator class="item" v-for="(item, idx) in twoColGoods" :key="idx">
+      <img :src="item.img_src">
+      <div class="wrap">
+        <span class="title">{{item.title}}</span>
+        <div class="line">
+          <div class="price"><span>￥</span>{{item.price}}</div>
+        </div>
+        <div class="sub"><del>￥{{item.oldPrice}}</del></div>
+      </div>
+    </navigator>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {},
+  props: ['twoColGoods']
+}
+</script>
+
+<style lang="scss">
+   // 商品列表
+  .two-clo-goods {
+    padding: 0 30rpx;
+    display: flex;
+    flex-flow: row wrap;
+    align-content: flex-start;
+    .item {
+      margin-top: 20rpx;
+      display: flex;
+      flex-direction: column;
+      width: 334rpx;
+      border-radius: 10rpx;
+      overflow: hidden;
+      background-color: #fff;
+      &:nth-of-type(2n) {
+        margin-left: 22rpx;
+      }
+      img {
+        width: 334rpx;
+        height: 334rpx;
+      }
+      .wrap {
+        padding: 0 24rpx 20rpx;
+        .title {
+          margin-top: 20rpx;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          overflow: hidden;
+          font-size: 30rpx;
+          line-height: 38rpx;
+        }
+        .line {
+          margin-top: 20rpx;
+          line-height: 1;
+          .price {
+            font-size: 28rpx;
+            span {
+              font-size: 18rpx;
+            }
+          }
+        }
+        .sub {
+          margin-top: 20rpx;
+          font-size: 18rpx;
+          line-height: 1;
+        }
+      }
+    }
+  }
+</style>
