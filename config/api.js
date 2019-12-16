a39dd6c96b7f0c2535d41ba109444a1e6f13ec27
Xing <963255230@qq.com>
2019-12-16 11:19:58 +0800

    添加mock接口

--- config/api.js
+++ config/api.js
@@ -1,6 +1,11 @@
-const ApiRootUrl = 'http://www.liulongbin.top:3005/api/'
+// const ApiRootUrl = 'http://www.liulongbin.top:3005/api/'
+const ApiRootUrl = 'https://www.fastmock.site/mock/'
 
 module.exports = {
+    HomeInfo: ApiRootUrl + 'e57832aa3c4a003731a9cd8234dec828/gugugou/api/home', // 主页
+    Cates: ApiRootUrl + 'e57832aa3c4a003731a9cd8234dec828/gugugou/api/cates', // 分类
     GoodsList: ApiRootUrl + 'getgoods', // 商品列表
-    GoodsShow: ApiRootUrl + 'getthumimages/' // 商品详情-轮播
+    GoodsShow: ApiRootUrl + 'getthumimages/', // 商品详情-轮播
+    SearchResult: ApiRootUrl + 'e57832aa3c4a003731a9cd8234dec828/gugugou/api/search_result', //  搜索结果
+    Collection: ApiRootUrl + 'e57832aa3c4a003731a9cd8234dec828/gugugou/api/collection' // 收藏列表
 }
