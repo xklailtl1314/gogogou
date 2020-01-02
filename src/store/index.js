d1462da8027d577e7336bfc02c5332ce296dedc3
Xing <963255230@qq.com>
2020-01-02 18:26:57 +0800

    购物车数组--vuex-state添加 本地获取数据

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state'
 import mutations from './mutation'
+import getters from './getter'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
+  getters,
   mutations
 })
