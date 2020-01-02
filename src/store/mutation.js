23fe3e0aae3ebc4f54113ce6e8d148e11ad3eeb1
Xing <963255230@qq.com>
2020-01-02 18:27:28 +0800

    vuex--购物车数据添加

--- src/store/mutation.js
+++ src/store/mutation.js
@@ -1,4 +1,19 @@
 const mutations = {
+  addToCar (state, goodsinfo) {
+    var flag = false
+    state.car.some(item => {
+      if (item.id == goodsinfo.id) { // 判断购物车中商品id相等，数量相加，否则添加商品
+        item.count += parseInt(goodsinfo.count)
+        flag = true
+        return true
+      }
+    })
 
+    if (!flag) {
+      state.car.push(goodsinfo)
+    }
+    // car 存本地数据
+    wx.setStorageSync('car', state.car)
+  }
 }
 export default mutations
