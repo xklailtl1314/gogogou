8f5b56967169f003a2b3f081a2029b780e178040
Xing <963255230@qq.com>
2020-01-03 16:21:52 +0800

    商品退款--未通过--添加

---
+++ src/pages/refundFailed/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="refund-failed">
+    <!-- 退款头部 -->
+    <refundHeader :refundMsg="refundMsg" :refundIcon="refundIcon"></refundHeader>
+    <!-- 退款信息 -->
+    <div class="refund-info">
+      <p class="tit">商家回复：</p>
+      <span class="sub">该产品不属于7天无理由退换货范围。</span>
+      <div class="wrap">
+        若您在
+        <span class="time">03天12小时59分59秒</span>
+        内未重新申请，则该退款自动关闭。
+      </div>
+    </div>
+    <!-- 退款详情、协商记录 -->
+    <refundTab></refundTab>
+    <!-- 按钮组 -->
+    <div class="refund-btn-wrap">
+      <div class="btn active">修改退款申请资料</div>
+      <div class="btn kefu">联系客服</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import refundHeader from '@/components/RefundHeader.vue' // 头部组件
+import refundTab from '@/components/RefundTab.vue' // tab切换组件
+export default {
+  data () {
+    return {
+      refundMsg: '您的退款申请未通过', // 头部传值
+      refundIcon: 2 // 头部
+    }
+  },
+  components: {
+    refundHeader,
+    refundTab
+  }
+}
+</script>
+
+<style lang="scss">
+page {
+  background-color: #eee;
+}
+.refund-failed {
+  .refund-info {
+    padding: 30rpx;
+    background-color: #fff;
+    .tit {
+      font-size: 26rpx;
+      color: #333;
+    }
+    .sub {
+      margin-top: 20rpx;
+      font-size: 26rpx;
+      color: #333;
+    }
+    .wrap {
+      margin-top: 50rpx;
+      font-size: 24rpx;
+      color: #666;
+      .time {
+        margin: 0 10rpx;
+        color: #DA5998;
+      }
+    }
+  }
+
+  .refund-btn-wrap {
+    margin-top: 120rpx;
+    .btn {
+      margin: 30rpx 30rpx 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 28rpx;
+      line-height: 1;
+      color: #fff;
+      height: 70rpx;
+      border-radius: 35rpx;
+      background-color: #999;
+      &.active {
+        background:linear-gradient(90deg,rgba(210,54,129,1) 0%,rgba(181,28,102,1) 100%);
+      }
+      &.kefu {
+        margin-bottom: 60rpx;
+      }
+    }
+  }
+}
+</style>
