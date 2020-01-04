38867267598e1d9cef380762abf83cbf6702915d
Xing <963255230@qq.com>
2020-01-04 14:03:22 +0800

    收藏页面--右划删除--添加

--- src/pages/collection/index.vue
+++ src/pages/collection/index.vue
@@ -1,21 +1,27 @@
 <template>
+  <!-- 我的收藏页 -->
   <div class="my-collection">
-    <!-- 我的收藏页 -->
     <!-- 我的列表 -->
-    <div class="my-list" v-if="on_off">
-      <navigator v-for="(item, idx) in goodsList" :key="idx" class="goods-list" hover-class="none">
+    <ul class="my-list" v-if="goodsList.length!=0">
+      <li v-for="(item, idx) in goodsList" :key="idx"
+          class="goods-list"
+          @touchstart="touchStart($event)"
+          @touchend="touchEnd($event,idx)"
+          :data-type="item.type">
         <img :src="item.img_src" class="img">
         <div class="right">
           <div class="title">{{item.title}}</div>
-          <div class="sub">{{item.sub}}</div>
+          <div class="sub">{{item.sub}}人收藏</div>
           <div class="tip" v-show="item.tip" ><span>{{item.tip}}</span></div>
           <div class="bottom">
             <span class="price"><small>￥</small>{{item.price}}</span>
             <span class="btn">找相似</span>
           </div>
         </div>
-      </navigator>
-    </div>
+        <div class="other-item delete" @click="delect(idx)">删除</div>
+      </li>
+    </ul>
+
     <!-- 没有收藏 -->
     <div class="empty" v-else>
       <i class="iconfont icon-gouwudai"></i>
@@ -35,23 +41,63 @@ export default {
   data () {
     return {
       on_off: true, // 判断是否为空
-      oLink: '../index/main', // 空状态商城链接
-      goodsList: [] // 收藏列表
+      oLink: '../mall/main', // 空状态商城链接
+      goodsList: [ // 收藏列表
+        {
+          id: 1,
+          img_src: '/static/tmp/b007.jpeg',
+          title: 'Razer 雷蛇|宝可梦 皮卡丘限定款鼠标+鼠标垫套装科奥',
+          sub: 278,
+          tip: '比收藏时降价3.00元',
+          price: '6.9',
+          type: 0
+        },
+        {
+          id: 2,
+          img_src: '/static/tmp/b005.jpg',
+          name: 'Razer 雷蛇|宝可梦 皮卡丘限定款鼠标+鼠标垫套装科奥',
+          sub: 278,
+          tip: '比收藏时降价3.00元',
+          price: '6.9',
+          type: 0
+        }
+      ]
     }
   },
   methods: {
-    getCollection () { // 获取收藏列表
-      util.request(api.Collection).then(res => {
-        // console.log(res)
-        if (res.status == 200) {
-          this.goodsList = res.message
-          // console.log(this.goodsList)
+    touchStart (e) { // 滑动开始
+      // 获取移动距离，可以通过打印出e，然后分析e的值得出
+      this.startX = e.mp.changedTouches[0].clientX
+      // console.log(this.startX)
+    },
+    touchEnd (e, index) { // 滑动结束
+      // 获取移动距离
+      this.endX = e.mp.changedTouches[0].clientX
+      if (this.startX - this.endX > 10) {
+        for (let i = 0; i < this.goodsList.length; i++) {
+          this.goodsList[i].type = 0
         }
-      })
+        this.goodsList[index].type = 1
+      } else if (this.startX - this.endX < -10) {
+        for (let i = 0; i < this.goodsList.length; i++) {
+          this.goodsList[i].type = 0
+        }
+      }
+    },
+    // 删除
+    delect (index) {
+      this.goodsList.splice(index, 1)
     }
+    // getCollection () { // 获取收藏列表
+    //   util.request(api.Collection).then(res => {
+    //     if (res.status == 200) {
+    //       this.goodsList = res.message
+    //     }
+    //   })
+    // }
   },
   created () {
-    this.getCollection()
+    // this.getCollection()
   }
 }
 </script>
@@ -62,23 +108,27 @@ page {
 }
 .my-collection {
   .my-list {
-    // margin-top: 20rpx;
-    padding: 20rpx 30rpx 0;
+    padding: 20rpx 0 0;
     display: flex;
     flex-direction: column;
+    width: 100%;
     background-color: #fff;
     border-top: 1rpx solid #eee;
-    .goods-list {
-      margin-bottom: 20rpx;
+    li.goods-list {
+      margin: 0 0 20rpx 30rpx;
       display: flex;
+      width: 820rpx;
       height: 240rpx;
+      -webkit-transition: all 0.2s;
+      transition: all 0.2s;
       .img {
         width: 240rpx;
         height: 240rpx;
         border-radius: 10rpx;
       }
       .right {
-        margin-left: 20rpx;
+        margin: 0 30rpx 0 20rpx;
+        width: 430rpx;
         display: flex;
         flex-direction: column;
         flex: 1;
@@ -131,6 +181,24 @@ page {
           }
         }
       }
+      .other-item {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100rpx;
+        height: auto;
+        font-size: 24rpx;
+        color: #fff;
+        &.delete {
+          background-color: #B51C66;
+        }
+      }
+    }
+    li[data-type="0"] {
+      transform: translate3d(0, 0, 0);
+    }
+    li[data-type="1"] {
+      transform: translate3d(-100rpx, 0, 0);
     }
   }
   // 空状态
