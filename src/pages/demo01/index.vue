3d522755ada6f8a30d85eb40742769ca21ec1677
Xing <963255230@qq.com>
2020-01-04 21:19:31 +0800

    demo01--修改

--- src/pages/demo01/index.vue
+++ src/pages/demo01/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="container">
+  <div>
+    <div class="bg">
+      <img src="/static/tmp/zhibo.jpeg">
+    </div>
 
+    <div class="mask">
+      <view class='empty' :style="{bottom: Bottom}">
+        <view class='inputarea'>
+          <input adjust-position="{false}" @focus="foucus" @blur="blur" />
+        </view>
+      </view>
+    </div>
+
+    <!-- <view class='empty' :style="{bottom: bottom}">
+      <view class='inputarea'>
+        <input />
+      </view>
+    </view> -->
   </div>
 </template>
 
@@ -8,11 +24,49 @@
 export default {
   data () {
     return {
-
+      Bottom: 0
+    }
+  },
+  methods: {
+    // 输入聚焦
+    foucus: (e) => {
+      this.Bottom = e.mp.detail.height * 2 + 'rpx'
+      console.log('输入聚焦' + this.Bottom)
+    },
+    // 失去聚焦
+    blur: (e) => {
+      this.Bottom = 0
+      console.log('失去聚焦:' + this.Bottom)
     }
   }
 }
 </script>
-<style scoped lang="scss">
 
+<style lang="scss">
+.bg {
+  width: 100vw;
+  height: 100vh;
+  img {
+    width: 100vw;
+    height: 100vh;
+  }
+}
+.mask {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.6);
+  position: absolute;
+  top: 0;
+}
+.empty {
+  width: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+}
+.inputarea {
+  // height: 300rpx;
+  background: #fff;
+  position: relative;
+}
 </style>
