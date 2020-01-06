914224c96984e0885d4cbda92be87f6ba2a890b3
Xing <963255230@qq.com>
2020-01-06 20:38:57 +0800

    demo01--修改

--- src/pages/demo01/index.vue
+++ src/pages/demo01/index.vue
@@ -1,22 +1,25 @@
 <template>
-  <div>
-    <div class="bg">
-      <img src="/static/tmp/zhibo.jpeg">
+  <div class="integralPage">
+    <div class="itemList">
+      <div class="itemBox" v-for="(item, index) in list" :key="index">
+        <div class="info">
+          <div class="title">{{item.title}}</div>
+          <div class="time">{{item.time}}</div>
+        </div>
+        <div class="num">{{item.num}}</div>
+      </div>
     </div>
-
-    <div class="mask">
-      <view class='empty' :style="{bottom: Bottom}">
-        <view class='inputarea'>
-          <input adjust-position="{false}" @focus="foucus" @blur="blur" />
-        </view>
-      </view>
+    <div class="laodMore">
+      <div v-show="logStatus === 0">
+        <div>上拉加载</div>
+      </div>
+      <div v-show="logStatus === 1">
+        <div>正在加载</div>
+      </div>
+      <div v-show="logStatus === 2">
+        <div>没有更多数据了</div>
+      </div>
     </div>
-
-    <!-- <view class='empty' :style="{bottom: bottom}">
-      <view class='inputarea'>
-        <input />
-      </view>
-    </view> -->
   </div>
 </template>
 
@@ -24,49 +27,99 @@
 export default {
   data () {
     return {
-      Bottom: 0
+      list: [],
+      listParam: {
+        page: 1
+      },
+      noData: false,
+      logStatus: 0
     }
   },
+  // 上拉加载，拉到底部触发
+  onReachBottom () {
+    this.onScroll()
+  },
+  created () {
+    setTimeout(() => {
+      this.list = [...new Array(20).keys()].map(item => ({ // 初始化数据
+        title: new Date() + '签到' + item,
+        time: '2019/8/13 15:23:22',
+        num: '+10'
+      }))
+      this.listParam.page += 1
+    }, 500)
+  },
   methods: {
-    // 输入聚焦
-    foucus: (e) => {
-      this.Bottom = e.mp.detail.height * 2 + 'rpx'
-      console.log('输入聚焦' + this.Bottom)
-    },
-    // 失去聚焦
-    blur: (e) => {
-      this.Bottom = 0
-      console.log('失去聚焦:' + this.Bottom)
+    onScroll () { // 如果是小程序, 不用监听滚动事件, 也不用户判断高度, 因为小程序有触底事件 onReachBottom , 直接在其中写请求逻辑即可
+      this.logStatus = 1
+      // 可以在这个地方给 innerHeight 添加适当的高度调整效果
+      if (this.noData === true) {
+        this.logStatus = 2
+        return false
+      }
+      setTimeout(() => {
+        const data = { // 请求回来的数据
+          rows: [...new Array(10).keys()].map(item => ({
+            title: new Date() + '签到' + item,
+            time: '2019/8/13 15:23:22',
+            num: '+10'
+          })),
+          count: 40 // 服务器上总数
+        }
+        if (this.list.length < data.count) { // 本地总数是否大于服务器总数
+          this.list = [...this.list, ...data.rows]
+          this.listParam.page = this.listParam.page + 1
+          this.logStatus = 0
+        } else {
+          this.logStatus = 2
+          this.noData = true
+        }
+      }, 500)
     }
   }
 }
 </script>
 
 <style lang="scss">
-.bg {
-  width: 100vw;
-  height: 100vh;
-  img {
-    width: 100vw;
-    height: 100vh;
+.integralPage {
+  font-size: 14px;
+  color: #101010;
+  // 分类列表
+  .itemList {
+    border-top: 1px solid #bbb;
+    margin-left: 12px;
+    margin-right: 12px;
+    .itemBox {
+      margin-left: 16px;
+      margin-right: 2px;
+      border-bottom: 1px solid #E8E8E8;
+      padding-top: 7px;
+      padding-bottom: 7px;
+      overflow: hidden;
+      .info {
+        float: left;
+        max-width: 80%;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        .time {
+          font-size: 10px;
+          color: #878686;
+        }
+      }
+      .num {
+        padding-top: 10px;
+        padding-right: 10px;
+        float: right;
+      }
+    }
   }
-}
-.mask {
-  width: 100%;
-  height: 100%;
-  background: rgba(0,0,0,0.6);
-  position: absolute;
-  top: 0;
-}
-.empty {
-  width: 100%;
-  position: absolute;
-  left: 0;
-  right: 0;
-}
-.inputarea {
-  // height: 300rpx;
-  background: #fff;
-  position: relative;
+  // 上拉加载
+  .laodMore {
+    padding-top: 8px;
+    padding-bottom: 8px;
+    color: #ccc;
+    text-align: center;
+  }
 }
 </style>
